65b4d41ecbd73553ca7aa53bf698615249f7051f
endrjuki <endrju.vilcmeiers@gmail.com>
2021-02-23 10:31:18 +0200

    .

--- todo.rb
+++ todo.rb
@@ -1,7 +1,99 @@
-require "sinatra"
-require "sinatra/reloader"
-require "tilt/erubis"
+# frozen_string_literal: true
 
-get "/" do
-  erb "You have no lists.", layout: :layout
+require 'sinatra'
+require 'sinatra/content_for'
+require 'sinatra/reloader'
+require 'tilt/erubis'
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
+get '/' do
+  redirect '/lists'
+end
+
+# View list of lists
+get '/lists' do
+  @lists = session[:lists]
+  erb :lists, layout: :layout
+end
+
+# Render the new list form
+get '/lists/new' do
+  erb :new_list, layout: :layout
+end
+
+# Return an error message if the name is invalid, returns nil otherwise
+def error_for_list_name(name)
+  if !(1..50).cover? name.size
+    'List name must be between 1 and 50 characters!'
+  elsif session[:lists].any? { |list| list[:name] == name }
+    'List name must be unique!'
+  end
+end
+
+# Create a new list
+post '/lists' do
+  list_name = params[:list_name].strip
+
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = 'The list has been created.'
+    redirect '/lists'
+  end
+end
+
+get '/lists/:id' do
+  id = params[:id].to_i
+  @list = session[:lists][id]
+  erb :list, layout: :layout
+end
+
+# Edit an existing todo list
+get '/lists/:id/edit' do
+  id = params[:id].to_i
+  @list = session[:lists][id]
+  erb :edit_list, layout: :layout
+end
+
+# Update an existing todo list
+post "/lists/:id" do  
+  list_name = params[:list_name].strip
+  id = params[:id].to_i
+  @list = session[:lists][id]
+
+  error = error_for_list_name(list_name)
+  if error    
+    session[:error] = error
+    erb :edit_list, layout: :layout
+  else
+    @list[:name] = list_name
+    session[:success] = 'The list has been updated.'
+    redirect "/lists/#{id}"
+  end
+end
+
+post "/lists/:id/destroy" do
+  id = params[:id].to_i
+  session[:lists].delete_at(id)
+  session[:success] = "The list has been deleted."
+  redirect "/lists"
+end
+
+post "/lists/:id/todos" do
+  id = params[:id].to_i
+  todo = params[:todo]
+  session[:success] = "Todo has been added."
+  session[:lists][id][:todos] << todo
+  redirect "/lists/:id"
 end
